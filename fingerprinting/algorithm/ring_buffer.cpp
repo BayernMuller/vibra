b02ccba9d34dc4582e0dd9b982fc4043b8f18954
BayernMuller <cjjunk29@gmail.com>
2023-04-24 11:53:37 +0900

    added ring buffer

---
+++ fingerprinting/algorithm/ring_buffer.cpp
@@ -0,0 +1,4 @@
+#include "ring_buffer.h"
+
+// do nothing
+// because the template class must be implemented with the class definition.
---
+++ fingerprinting/algorithm/ring_buffer.h
@@ -0,0 +1,48 @@
+#include <vector>
+
+template <typename T>
+class RingBuffer
+{
+public:
+    RingBuffer(std::size_t size);
+    ~RingBuffer();
+
+    void Append(const T& value);
+
+    std::uint32_t Size() const { return mSize; }
+    std::uint32_t NumWritten() const { return mNumWritten; }
+    std::uint32_t Position() const { return mPosition; }
+
+    T& operator[](std::uint32_t index)
+    {
+        return mVec[index];
+    }
+
+private:
+    std::uint32_t mSize;
+    std::uint32_t mNumWritten;
+    std::uint32_t mPosition;
+    std::vector<T> mVec;
+};
+
+template <typename T>
+RingBuffer<T>::RingBuffer(std::size_t size)
+    : mVec(size)
+    , mSize(size)
+    , mNumWritten(0)
+    , mPosition(0)
+{
+}
+
+template <typename T>
+RingBuffer<T>::~RingBuffer()
+{
+}
+
+template <typename T>
+void RingBuffer<T>::Append(const T& value)
+{
+    mVec[mPosition] = value;
+    mPosition = (mPosition + 1) % mSize;
+    mNumWritten++;
+}
