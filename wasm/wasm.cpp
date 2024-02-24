131b23c4fa693211eb0e66bb8af77af638fd62e2
BayernMuller <cjjunk29@gmail.com>
2024-02-24 21:30:45 +0900

    feat: expand WASM API to include GetFingerprint and GetSampleMs

--- wasm/wasm.cpp
+++ wasm/wasm.cpp
@@ -9,9 +9,13 @@ extern "C" {
 
 #define DBG(x) std::cout << "[" << __FUNCTION__ << "] " << #x << ": " << x << std::endl;
 
-std::string gSignature;
+struct SignatureWrapper
+{
+    std::string uri;
+    unsigned int samplems;
+};
 
-char* EMSCRIPTEN_KEEPALIVE GetSignature(char* raw_wav, int wav_data_size)
+SignatureWrapper* EMSCRIPTEN_KEEPALIVE GetSignature(char* raw_wav, int wav_data_size)
 {
     Wav wav(raw_wav, wav_data_size);
 
@@ -26,8 +30,21 @@ char* EMSCRIPTEN_KEEPALIVE GetSignature(char* raw_wav, int wav_data_size)
     generator.AddSampleProcessed(LOW_QUALITY_SAMPLE_RATE * ((int)duaration / 2) - 6);
 
     Signature signature = generator.GetNextSignature();
-    gSignature = signature.GetBase64Uri();
-    return const_cast<char*>(gSignature.c_str());
+    
+    static SignatureWrapper wrapper;
+    wrapper.uri = signature.GetBase64Uri();
+    wrapper.samplems = signature.NumberOfSamples() / signature.SampleRate() * 1000;
+    return &wrapper;
+}
+
+char* EMSCRIPTEN_KEEPALIVE GetFingerprint(SignatureWrapper* signature)
+{
+    return const_cast<char*>(signature->uri.c_str());
+}
+
+unsigned int EMSCRIPTEN_KEEPALIVE GetSampleMs(SignatureWrapper* signature)
+{
+    return signature->samplems;
 }
 
 int main()
